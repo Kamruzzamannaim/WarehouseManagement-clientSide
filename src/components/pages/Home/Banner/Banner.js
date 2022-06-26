f9d774d9e7bc937cd9c9aa2eb8b5587f0c73a81b
Naim <kamruzzamannnaim901@gmail.com>
2022-06-26 17:35:31 +0600

    banner added

--- src/components/pages/Home/Banner/Banner.js
+++ src/components/pages/Home/Banner/Banner.js
@@ -1,8 +1,14 @@
-
+import './Banner.css';
 const Banner = () => {
     return (
         <div>
+           <div className="banner">
             
+            <div className="info px-2 py-5">
+                <h1 className='text-white '>Best Racing Bike <br /> In world</h1>
+                <button type="button" className="btn btn-outline-primary btn-lg ">See more</button>
+            </div>
+            </div> 
         </div>
     );
 };
